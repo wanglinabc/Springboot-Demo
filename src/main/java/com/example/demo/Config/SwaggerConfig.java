0d4639182210563dddbf87f902ce2b0e10f479e9
wanglinabc <wanglin@asiacrit.com>
2018-09-30 15:39:28 +0800

    优化目录结构

--- src/main/java/com/example/demo/Config/SwaggerConfig.java
+++ src/main/java/com/example/demo/Config/SwaggerConfig.java
@@ -1,17 +1,40 @@
 package com.example.demo.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @Configuration
-public class SwaggerConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.Controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    //构建 api文档的详细信息函数
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("车位链接口API")
+                //创建人
+                .contact(new Contact("We", "http://We.com", "We@163.com"))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("API 描述")
+                .build();
     }
 }
--- src/main/java/com/example/demo/Config/getApplicationConfig.java
+++ src/main/java/com/example/demo/Config/getApplicationConfig.java
@@ -3,6 +3,10 @@ package com.example.demo.Config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * 获取全局通用配置文件
+ */
+
 @Component
 @ConfigurationProperties(prefix = "com")
 public class getApplicationConfig {
